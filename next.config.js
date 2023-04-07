df34ad90b771709824d571dd48e33f3cba605400
hacKRD0 <dattakeshava@gmail.com>
2023-04-07 17:00:58 +0530

    Gallery Grid with 3rd party modules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.google.com', 'bit.ly', "images.unsplash.com", "c1.staticflickr.com", "c2.staticflickr.com", "c7.staticflickr.com", "c3.staticflickr.com", "c4.staticflickr.com", "c5.staticflickr.com", "c6.staticflickr.com", "c7.staticflickr.com", "c8.staticflickr.com", "c9.staticflickr.com",]
+  }
 }
 
 module.exports = nextConfig
