a05a627feb876d135aa80e9cc927053fd75decc2
nirav-actyv <niravjayesh.intern@actyv.com>
2023-04-09 19:09:54 +0530

    jersey-page

--- pages/jersey-wall.js
+++ pages/jersey-wall.js
@@ -1,7 +1,41 @@
+import JerseySingleSide from "@/components/JerseySingleSide";
+import purple from 'assets/Purple.png'
+import gold from 'assets/Gold.png'
+import pink from 'assets/Pink.png'
+
 export default function jerseyWall() {
+    const jerseyList = [
+        {
+            year: 2022,
+            image: purple
+        },
+        {
+            year: 2021,
+            image: gold
+        },
+        {
+            year: 2020,
+            image: pink
+        }];
     return (
-        <div className="px-6">
-            <h1>Jersey Wall here!</h1>
+        <div>
+            {
+                jerseyList.map((jersey) => {
+                return  <div className="p-10 rounded border-solid border-2 mb-20">
+                            <div className="mb-5 flex justify-center">
+                                <p className="text-xl">{jersey.year}</p>
+                            </div>
+                            <div className="grid grid-cols-2" >
+                                <div className="mr-20 w-fit transform transition duration-200 hover:scale-105">
+                                    <JerseySingleSide imgPath={jersey.image}/>
+                                </div>
+                                <div className="ml-20 w-fit transform transition duration-200 hover:scale-105">
+                                    <JerseySingleSide imgPath={jersey.image}/>
+                                </div>
+                            </div>
+                        </div>
+                })
+            }
         </div>
     );
 }
